e8d7d4ba454cc1bff5de72860f9edabf1f61a3eb
FrancescaAmeche <121627825+FrancescaAmeche@users.noreply.github.com>
2023-09-19 22:31:10 +0000

    refactoring

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -3,16 +3,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 
 const List = () => {
-    const [items, setItems] = useState("");
-    const [addToList, setAddToList] = useState([]);
-    const [showOnHover, setShowOnHover] = useState({display: "none"});
+    const [input, setInput] = useState("");
     const [tasks, setTasks] = useState([]);
+    const [showOnHover, setShowOnHover] = useState({display: "none"});
+    const [tasks2, setTasks2] = useState([]);
     const url = "https://playground.4geeks.com/apis/fake/todos/user/francescaameche";
     
     function fetchInfo () {
         fetch(url)
         .then((response) => response.json())
-        .then((data) => setTasks(data))
+        .then((data) => setTasks2(data))
     }
 
     function createUsername () {
@@ -28,31 +28,14 @@ const List = () => {
     }
 
     function modifyTasks () {
-        fetch(url, {
-            method: "PUT",
-            body: JSON.stringify([
-                {
-                    "done": false,
-                    "id": 1,
-                    "label": "Wash dishes"
-                },
-                {
-                    "done": false,
-                    "id": 2,
-                    "label": "Do laundry"
-                },
-                {
-                    "done": false,
-                    "id": 3,
-                    "label": "Sweep floors"
-                }
-            ]),
-            headers: {
-                "Content-Type": "application/json"
-            }
+            fetch(url, {
+                method: "PUT",
+                body: JSON.stringify([
+                    setTasks2(tasks2.concat([input]))
+                ])
             })
-        .then((response) => response.json())
-        .then((data) => console.log(data))
+            .then((response) => response.json())
+            .then((data) => setTasks2(data))
     }
 
     useEffect( () =>{
@@ -62,20 +45,20 @@ const List = () => {
 
     function addItemToList(e) {
         if (e.key === "Enter") {
-            setAddToList(addToList.concat([items]));
-            setItems("");
+            setTasks(tasks.concat([input]));
+            setInput("");
           }   
     }
-    
+
 	return (
             <div className="list">
                 <h1>To-Do List</h1>
                 <ul>
-                    <li><input value={items} type="text" className="form-control" placeholder="To-Do" onChange={(e) => {setItems(e.target.value)}} onKeyDown= {addItemToList}/></li>
-                    {addToList.map ((a, index) => (<li onMouseEnter={e => {setShowOnHover({display: "block"});}} onMouseLeave={e => {setShowOnHover({display: "none"})}}>{a}{""}<button className="removeItem" style={showOnHover} onClick={() => setAddToList(addToList.filter ((t, currentIndex) => index != currentIndex))}><FontAwesomeIcon icon={faX} /></button></li>))}
-                    {tasks.map((item, index) => (<li key={index} onMouseEnter={e => {setShowOnHover({display: "block"});}} onMouseLeave={e => {setShowOnHover({display: "none"})}}>{ item.label }<button className="removeItem" style={showOnHover} onClick={() => setTasks(tasks.filter ((t, currentIndex) => index != currentIndex))}><FontAwesomeIcon icon={faX} /></button></li>))}
+                    <li><input value={input} type="text" className="form-control" placeholder="To-Do" onChange={(e) => {setInput(e.target.value)}} onKeyDown= {addItemToList}/></li>
+                    {tasks.map ((item, index) => (<li key={index} onMouseEnter={e => {setShowOnHover({display: "block"});}} onMouseLeave={e => {setShowOnHover({display: "none"})}}>{ item }{""}<button className="removeItem" style={showOnHover} onClick={() => setTasks(tasks.filter ((t, currentIndex) => index != currentIndex))}><FontAwesomeIcon icon={faX} /></button></li>))}
+                    {tasks2.map((item, index) => (<li key={index} onMouseEnter={e => {setShowOnHover({display: "block"});}} onMouseLeave={e => {setShowOnHover({display: "none"})}}>{ item.label }<button className="removeItem" style={showOnHover} onClick={() => setTasks2(tasks2.filter ((t, currentIndex) => index != currentIndex))}><FontAwesomeIcon icon={faX} /></button></li>))}
                 </ul>
-                <p>{addToList.length} items left</p>
+                <p>{tasks.length + tasks2.length} items left</p>
                 <button className="username" onClick={createUsername}>Create Username</button>
             </div>
 	);
